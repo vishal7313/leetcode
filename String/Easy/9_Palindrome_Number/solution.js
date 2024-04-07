d290c2b4b98adee53b6ecb8bd9f5d03dec6bd004
Vishal <vishal.ranjan7777@gmail.com>
2024-04-06 23:40:47 -0500

    Solve question # 9

---
+++ String/Easy/9_Palindrome_Number/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let str=x.toString()
+    let i=0
+    let j=str.length-1
+    while(i<j){
+        if(str[i]!==str[j]) {
+            return false
+        }
+        i++
+        j--
+    }
+    return true
+};
